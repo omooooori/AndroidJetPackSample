ad8de20365b10b41f4283e1e836471d301262c33
ora1027 <r.shimizu18020@gmail.com>
2020-03-05 00:55:19 +0900

    Set up navigation. Add FloatingActionButton.

--- app/src/main/java/com/android/omori/androidjetpacksample/view/MainActivity.kt
+++ app/src/main/java/com/android/omori/androidjetpacksample/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.omori.androidjetpacksample
+package com.android.omori.androidjetpacksample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.omori.androidjetpacksample.R
 
 class MainActivity : AppCompatActivity() {
 
