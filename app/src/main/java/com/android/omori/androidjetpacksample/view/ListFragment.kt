e8c80c1adecc7c0648de6e1fcfad5c8e5cb6c6cd
ora1027 <r.shimizu18020@gmail.com>
2020-03-05 21:53:50 +0900

    Implement arguments transition and animation.

--- app/src/main/java/com/android/omori/androidjetpacksample/view/ListFragment.kt
+++ app/src/main/java/com/android/omori/androidjetpacksample/view/ListFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import com.android.omori.androidjetpacksample.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +23,17 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetail.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            action.dogUuid = 5
+            Navigation.findNavController(it).navigate(action)
+//            view?.findNavController()?.navigate(R.id.actionDetailFragment)
+        }
     }
 
 }
