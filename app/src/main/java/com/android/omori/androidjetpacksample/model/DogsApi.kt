7a195ea29c5e8a0d4da0fd6ee0522571932ba131
ora1027 <r.shimizu18020@gmail.com>
2020-03-10 00:07:02 +0900

    Get remote data using api

--- app/src/main/java/com/android/omori/androidjetpacksample/model/DogsApi.kt
+++ app/src/main/java/com/android/omori/androidjetpacksample/model/DogsApi.kt
@@ -1,4 +1,10 @@
 package com.android.omori.androidjetpacksample.model
 
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface DogsApi {
+    @GET("DevTides/DogsApi/master/dogs.json")
+    fun getDogs() : Single<List<DogBreed>>
 }
