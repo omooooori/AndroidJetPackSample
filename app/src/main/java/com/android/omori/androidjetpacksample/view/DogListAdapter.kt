8cfcad609374554a3dda22c70fe76445dde247f3
ora1027 <r.shimizu18020@gmail.com>
2020-03-28 12:05:15 +0900

    DataBinding for text.

--- app/src/main/java/com/android/omori/androidjetpacksample/view/DogListAdapter.kt
+++ app/src/main/java/com/android/omori/androidjetpacksample/view/DogListAdapter.kt
@@ -3,9 +3,11 @@ package com.android.omori.androidjetpacksample.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.android.omori.androidjetpacksample.R
+import com.android.omori.androidjetpacksample.databinding.ItemDogBinding
 import com.android.omori.androidjetpacksample.model.DogBreed
 import com.android.omori.androidjetpacksample.util.getProgressDrawable
 import com.android.omori.androidjetpacksample.util.loadImage
@@ -23,7 +25,8 @@ class DogListAdapter(val dogList: ArrayList<DogBreed>) : RecyclerView.Adapter<Do
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
         val inflater : LayoutInflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_dog, parent, false)
+//        val view = inflater.inflate(R.layout.item_dog, parent, false)
+        var view = DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
         return DogViewHolder(view)
     }
 
@@ -32,19 +35,20 @@ class DogListAdapter(val dogList: ArrayList<DogBreed>) : RecyclerView.Adapter<Do
     }
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
-        holder.view.text_view_dog_name.text = dogList[position].dogBreed
-        holder.view.text_view_dog_life_span.text = dogList[position].lifeSpan
-        holder.view.setOnClickListener {
-            val action = ListFragmentDirections.actionDetailFragment()
-            action.dogUuid = dogList[position].uuid
-            Navigation.findNavController(it).navigate(action)
-        }
-        holder.view.image_view_dog.loadImage(
-            dogList[position].imageUrl,
-            getProgressDrawable(holder.view.image_view_dog.context)
-        )
+        holder.view.dog = dogList[position]
+//        holder.view.text_view_dog_name.text = dogList[position].dogBreed
+//        holder.view.text_view_dog_life_span.text = dogList[position].lifeSpan
+//        holder.view.setOnClickListener {
+//            val action = ListFragmentDirections.actionDetailFragment()
+//            action.dogUuid = dogList[position].uuid
+//            Navigation.findNavController(it).navigate(action)
+//        }
+//        holder.view.image_view_dog.loadImage(
+//            dogList[position].imageUrl,
+//            getProgressDrawable(holder.view.image_view_dog.context)
+//        )
     }
 
-    class DogViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
 
 }
