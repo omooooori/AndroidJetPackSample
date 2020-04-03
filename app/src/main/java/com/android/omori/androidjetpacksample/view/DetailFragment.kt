2a9c13f97ac2e526c58dbaf86ad6780740211ce8
ora1027 <r.shimizu18020@gmail.com>
2020-04-03 21:11:26 +0900

    Implement Palette.

--- app/src/main/java/com/android/omori/androidjetpacksample/view/DetailFragment.kt
+++ app/src/main/java/com/android/omori/androidjetpacksample/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.android.omori.androidjetpacksample.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,13 +11,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
 
 import com.android.omori.androidjetpacksample.R
 import com.android.omori.androidjetpacksample.databinding.FragmentDetailBinding
+import com.android.omori.androidjetpacksample.model.DogPalette
 import com.android.omori.androidjetpacksample.util.getProgressDrawable
 import com.android.omori.androidjetpacksample.util.loadImage
 import com.android.omori.androidjetpacksample.viewmodel.DetailViewModel
 import com.android.omori.androidjetpacksample.viewmodel.ListViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
@@ -57,8 +64,30 @@ class DetailFragment : Fragment() {
 //                text_view_dog_temperament_detail.text = dog.temperament
 //                text_view_dog_life_span_detail.text = dog.lifeSpan
 //                context?.let { image_view_dog_detail.loadImage(dog.imageUrl, getProgressDrawable(it)) }
+
+                it.imageUrl?.let {
+                    setupBackgroundColor(it)
+                }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url : String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            val myPalette = DogPalette(intColor)
+                            dataBinding.palette = myPalette
+                        }
+                }
+            })
     }
 
 }
